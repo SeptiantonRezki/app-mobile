2316f6a2f088c4c8dcd15fd7ba1bfecfdd8c4710
Septianton Rezki Rahmatulloh <61791770+SeptiantonRezki@users.noreply.github.com>
2021-09-24 16:34:32 +0700

    CRUD Interface wiht file done

--- configuration/database.js
+++ configuration/database.js
@@ -3,7 +3,6 @@ const mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  // password: "8%tmnN!t7",
   password: "",
   database: "db_ads",
 });
